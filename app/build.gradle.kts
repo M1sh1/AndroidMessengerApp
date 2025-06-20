88d2ece6a0bd55ec20cfd1a2315c8c552b81101f
NinoTchumburidze <ninotch03@gmail.com>
2025-06-20 22:04:34 +0400

    Search and Homepages added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,10 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation ("com.google.android.material:material:1.9.0")
+    implementation ("androidx.appcompat:appcompat:1.6.1")
+    implementation ("androidx.coordinatorlayout:coordinatorlayout:1.2.0")
+    implementation ("androidx.recyclerview:recyclerview:1.3.2")
+    implementation ("com.google.android.material:material:1.11.0")
 }
